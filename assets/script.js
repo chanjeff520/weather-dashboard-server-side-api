a7b474d460234ba7019165d3fad873090898b0fd
chanjeff520 <chanjeff520@gmail.com>
2022-10-11 03:47:29 -0400

    renders current time

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,9 @@ function validate(cityInfo){
     if(cityInfo === ""){
         alert("Please input a city");
         return false;
+    }else if(city.includes(cityInfo)){
+        alert("Please input a different City that's not in the saved area");
+        return false
     }
     return true;
 }
@@ -16,7 +19,7 @@ function validate(cityInfo){
 //not finished exclude hour
 function getApi(cityinfo){
     var weatherApiLink = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityinfo +"&units=imperial&appid=580736b4e81d6ca498d989b1ea6ba5b1";
-
+    //var currentWeatherApi
     
     fetch(weatherApiLink)
     .then(function(response){
@@ -29,6 +32,7 @@ function getApi(cityinfo){
     })
     .then(function(data){
         console.log(data);
+        renderWeather(data);
     })
 }
 
@@ -40,6 +44,8 @@ function makeCityBtn(){
         newCityBtn.setAttribute("class", "mx-auto mb-1 btn-block")
         newCityBtn.textContent = cityInfo.value;
         newCityBtn.onclick = onclickHistoryBtn ;
+
+        city.push(newCityBtn.textContent);
 
         document.getElementById("saved-cities").appendChild(newCityBtn);
     }
@@ -56,8 +62,28 @@ function renderCity(){
     getApi(cityInfo)
 }
 
+//renders current weather
 function renderWeather(data){
+    var currentWeatherEl = document.getElementById("current-weather");
+    var cityName = document.createElement("h2");
+    var icon = document.createElement("img");
+    var temp = document.createElement("p");
+    var wind = document.createElement("p");
+    var humidity = document.createElement("p");
 
+    cityName.textContent = data.city.name + " (" + moment.unix(data.list[0].dt).format("MM/D/YYYY") + ")";
+    cityName.setAttribute("class", "d-inline");
+    icon.setAttribute("src", "http://openweathermap.org/img/w/"+ data.list[0].weather[0].icon + ".png");
+    icon.setAttribute("class","d-inline");
+    temp.textContent = "Temperature: "+data.list[0].main.temp + "°F";
+    wind.textContent = "Wind speed: "+data.list[0].wind.speed + " mph";
+    humidity.textContent = "Humidity: "+data.list[0].main.humidity + "%";
+
+    currentWeatherEl.appendChild(cityName);
+    currentWeatherEl.appendChild(icon);
+    currentWeatherEl.appendChild(temp);
+    currentWeatherEl.appendChild(wind);
+    currentWeatherEl.appendChild(humidity);
 }
 
 function loadBtns(){
